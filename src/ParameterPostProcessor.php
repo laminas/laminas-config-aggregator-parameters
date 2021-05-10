5fb68e9d0a6687ab5d048eaa827565c2d5eec1a0
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2021-05-10 02:05:39 -0400

    Remove file headers
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/ParameterPostProcessor.php
+++ src/ParameterPostProcessor.php
@@ -1,11 +1,5 @@
 <?php
 
-/**
- * @see       https://github.com/laminas/laminas-config-aggregator-parameters for the canonical source repository
- * @copyright https://github.com/laminas/laminas-config-aggregator-parameters/blob/master/COPYRIGHT.md
- * @license   https://github.com/laminas/laminas-config-aggregator-parameters/blob/master/LICENSE.md New BSD License
- */
-
 declare(strict_types=1);
 
 namespace Laminas\ConfigAggregatorParameters;
